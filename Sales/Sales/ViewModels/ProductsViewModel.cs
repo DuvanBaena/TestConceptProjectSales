5d1620a39b7c55e3b724ec4c366caefbd484adb7
duvan baena <djbpuerta@hotmail.com>
2019-07-13 13:15:54 -0500

    Structure project

---
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace Sales.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Sales.Common.Models;
+    using Sales.Services;
+    using Xamarin.Forms;
+
+    public class ProductsViewModel  : BaseViewModel
+    {
+        #region Atribute
+
+        private ApiService apiServices;
+               
+        #endregion
+
+        #region Properties
+
+        private ObservableCollection<Product> products;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set { this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiServices = new ApiService();
+            this.LoadProducts();
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiServices.GetList<Product>("https://salesapitest.azurewebsites.net", "/api", "/Products");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+
+            this.Products = new ObservableCollection<Product>(list);
+        }
+        #endregion
+       
+
+    }
+}
