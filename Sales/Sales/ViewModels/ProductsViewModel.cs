3e36de1c5099f39ed7dc44686d41ab7ce589f49f
duvan baena <djbpuerta@hotmail.com>
2019-07-13 21:33:02 -0500

    Validation internet

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
     using Sales.Common.Models;
     using Sales.Services;
     using Xamarin.Forms;
@@ -12,7 +14,9 @@
         #region Atribute
 
         private ApiService apiServices;
-               
+
+        private bool isRefreshing;
+
         #endregion
 
         #region Properties
@@ -25,6 +29,15 @@
             set { this.SetValue(ref this.products, value); }
         }
 
+
+        public bool IsRefreshing
+        {
+                
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+
+
         public ProductsViewModel()
         {
             this.apiServices = new ApiService();
@@ -33,10 +46,25 @@
 
         private async void LoadProducts()
         {
-            var response = await this.apiServices.GetList<Product>("https://salesapitest.azurewebsites.net", "/api", "/Products");
+            this.IsRefreshing = true;
 
+            var connection = await this.apiServices.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+                return;
+            }
+
+            // the url of the Api is saved in a resource dictionary
+
+            var url = Application.Current.Resources["UrlApi"].ToString();
+            var response = await this.apiServices.GetList<Product>(url, "/api", "/Products");
+            
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
@@ -44,9 +72,22 @@
             var list = (List<Product>)response.Result;
 
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
         }
         #endregion
-       
 
+        #region Command
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+
+            }
+            
+        }
+
+        #endregion
     }
 }
